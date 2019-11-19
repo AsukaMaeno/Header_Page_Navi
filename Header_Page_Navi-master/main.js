486fce2c5685c36ab70dc8e228a4195a5944887a
AsukaMaeno <57939276+AsukaMaeno@users.noreply.github.com>
2019-11-19 17:50:15 +0900

    Add files via upload

---
+++ Header_Page_Navi-master/main.js
@@ -0,0 +1,7 @@
+var bar = document.getElementById("nav");
+
+const parent = document.querySelector(".gb_Wc");
+parent.insertBefore(bar, parent.firstChild);    
+
+const search_bar = document.querySelector(".RNNXgb");
+search_bar.style.width = "550px";
